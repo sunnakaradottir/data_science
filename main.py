fa79cfb3b49976073099550dc21e541756f117a2
hlynurblaer <hlynurblaer@gmail.com>
2025-11-19 23:58:33 +0100

    valdi k

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.data import load_base_data, fit_and_save_scaler, load_scaler, prepare_tune_split
 from src.cluster import load_kmeans_model, fit_k_means_and_save
-from src.train import run
+# from src.train import run
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,22 +27,22 @@ def main_setup():
     - create base and tune splits
     - fit scaler on all normal transactions (minus ones in tune set ca. 2500 samples)
     """
-    prepare_tune_split(times=5)
+    prepare_tune_split(times=4)
     fit_and_save_scaler(path_to_data='../data/base_data.csv')
     fit_k_means_and_save(n_clusters=3)
     
 
 
-def main_single_cluster():
-    X, labels = prep()
-    run(X, labels, cluster_id=2, sweep=True)
+# def main_single_cluster():
+#     X, labels = prep()
+#     run(X, labels, cluster_id=2, sweep=True)
 
 
 def main():
     # Uncomment to run setup (create splits and fit scaler)
-    # main_setup()
+    main_setup()
 
-   main_single_cluster()
+   #main_single_cluster()
 
 
 if __name__ == "__main__":
